ceab5411e5c80a85eee691d477c83a6a86db5664
Rezoana Rabiul <tasbihatanvir@gmail.com>
2022-09-13 06:55:25 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-
-
 /**
  *
  **main - function prints text as output
@@ -11,17 +9,14 @@
  **Return:return 0
  *
  **/
-
 int main(void)
-
 {
-
-		char school[10] = "_putchar";
-		int i;
-		for (i = 0; i < 8; i++)
-		{
-			_putchar(school[i]);
-		}
-		_putchar(10);
-		return (0);
+	char school[10] = "_putchar";
+	int i;
+	for (i = 0; i < 8; i++)
+	{
+	    _putchar(school[i]);
+	}
+	_putchar(10);
+	return (0);
 }		
