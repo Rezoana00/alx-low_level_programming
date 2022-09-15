61266d946bb11e53940eff21582a44f31eb6bd1f
Rezoana Rabiul <tasbihatanvir@gmail.com>
2022-09-15 03:05:29 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,9 +6,16 @@
  * Return: Always 0.
  *
  **/
-int main(void)
+void print_numbers(void)
 {
-	print_numbers();
-	    return (0);
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		_putchar (i + '0');
+		i++;
+	}
+	_putchar ('\n');
 }
 
